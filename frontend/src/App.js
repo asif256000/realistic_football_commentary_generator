1ab9fce353a13300fe6bd1f5b9fcb8accc33ea96
cpiyush <cpiyush854@rlogin.cs.vt.edu>
2024-04-23 16:35:28 -0400

    Proper summary created in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import './App.css'; 
+import data from './generated_summaries.json';
 import myImage from './images/_2b12a71e-43f5-45b3-bafe-cf1dca77ef70.jpg'
 import image1 from './images/_4c3303d2-4e16-4254-82b7-387b756a0834.jpg';
 import image2 from './images/_070f2f46-dd49-4fca-9e0e-4d418d0662db.jpg';
@@ -10,46 +11,58 @@ import image5 from './images/_c06113d9-90c9-41ce-83a1-ea689aef4c88.jpg';
 import image6 from './images/_f6815fda-748d-4930-9fba-6418241b8417.jpg';
 import image7 from './images/_fb64efe5-6225-45df-ab45-50076e1735c5.jpg';
 
+const OpenAI = require("openai");
+const openai = new OpenAI({
+    apiKey: "", 
+    dangerouslyAllowBrowser: true,
+});
+
 const App = () => {
 
     // State variables
     const [games, setGames] = useState([]);
-    const [selectedYear, setSelectedYear] = useState('2012'); // Default year
+    const [selectedLeague, setselectedLeague] = useState('england'); // Default year
     const [stringArray, setStringArray] = useState([]);
+    const [summaries, setSummaries] = useState([]);
+    const [paragraphs, setParagraphs] = useState([]);
 
-    const handleYearChange = (event) => {
-        setSelectedYear(event.target.value);
+
+    const handleLeagueChange = (event) => {
+        setselectedLeague(event.target.value);
     };
     
+    // on click event for the generate commentary button
     const generate_commentary = async() => {
-        console.log(stringArray); // Trigger click event on the gen_commentary button
 
-         const commentaryText = stringArray.join('\n');
+        const commentaryText = stringArray.join('\n');
 
-         // Update the commentary <div> with the concatenated text
-         document.querySelector(".text").innerText = commentaryText;
-        try {
-            // Generate images for each text in stringArray
-            const imageUrls = await Promise.all(stringArray.map(prompt => generateImages(prompt)));
+        // Update the commentary <div> with the concatenated text
+        document.querySelector(".text").style.display = "none";
+        setParagraphs(String(summaries).split('\n\n'));
+
+        // try {
+        //     // Generate images for each text in stringArray
+        //     const imageUrls = await Promise.all(stringArray.map(prompt => generateImages(prompt)));
             
-            // Append the generated images to the image_list div
-            const imageListDiv = document.querySelector(".image-list");
-            imageUrls.forEach(imageUrl => {
-                const img = document.createElement("img");
-                img.src = imageUrl;
-                img.alt = "Generated Image";
-                img.className = "highlight";
-                imageListDiv.appendChild(img);
-            });
-        } catch (error) {
-            console.error("Error generating commentary:", error);
-        }
+        //     // Append the generated images to the image_list div
+        //     const imageListDiv = document.querySelector(".image-list");
+        //     imageUrls.forEach(imageUrl => {
+        //         const img = document.createElement("img");
+        //         img.src = imageUrl;
+        //         img.alt = "Generated Image";
+        //         img.className = "highlight";
+        //         imageListDiv.appendChild(img);
+        //     });
+        // } catch (error) {
+        //     console.error("Error generating commentary:", error);
+        // }
     }
 
-    // New function to handle games select dropdown change
+    // Function to handle games select dropdown change
     const handleGamesChange = (event) => {
         const selectedGameId = event.target.value;
-        //console.log("Selected Game ID:", selectedGameId);
+ 
+        setSummaries(data[selectedGameId]);
     
         // Parse the filtered.csv file
         Papa.parse("filtered.csv", {
@@ -58,13 +71,9 @@ const App = () => {
             complete: (result) => {
                 // Filter rows based on matching id_odsp and text containing "goal"
                 const matchingRows = result.data.filter(row => row.id_odsp === selectedGameId && row.text.toLowerCase().includes('goal'));
-                // Log the matching rows
                 const texts = matchingRows.map(row => row.text);
-                console.log("Matching Rows:", matchingRows);
-                //console.log("Texts:", texts);
-                // Update the state with the array of texts
                 setStringArray(texts);
-                //console.log(stringArray);
+
             },
             error: (error) => {
                 console.error("Error parsing filtered.csv:", error);
@@ -80,29 +89,30 @@ const App = () => {
             header: true,
             complete: (result) => {
                 const sortedGames = result.data.sort((a, b) => a.season.localeCompare(b.season));
-                const filteredGames = sortedGames.filter(game => game.season === selectedYear);
+                const filteredGames = sortedGames.filter(game => game.country === selectedLeague);
                 setGames(filteredGames);
             },
             error: (error) => {
                 console.error("Error parsing CSV:", error);
             }
         });
-    }, [selectedYear]);
+    }, [selectedLeague]);
 
+    // Generates the images based on given prompt
     const generateImages = async (prompt) => {
         try {
             const response = await openai.images.generate({
                 model: "dall-e-3",
-                prompt: prompt, // Use the prompt parameter here
+                prompt: prompt,
                 n: 1,
                 size: "1024x1024",
             });
             const imageUrl = response.data[0].url;
             console.log(imageUrl);
-            return imageUrl; // Return the imageUrl
+            return imageUrl; 
         } catch (error) {
             console.error("Error generating image:", error);
-            return null; // Return null in case of error
+            return null; 
         }
     };
 
@@ -129,42 +139,30 @@ const App = () => {
                     <p className="text" style={{ margin: '3px' }}>
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                     </p>
+                    {paragraphs.map((paragraph, index) => (
+                        <p key={index}>{paragraph}</p>
+                    ))}
+
                     <button className="play_audio">Play Audio</button>
                 </div>
 
                 <div className="options">
-                    <div className="option-group">
-                        <label htmlFor="years">Year:</label>
-                        <select name="years" id="years" className="years" onChange={handleYearChange}>
-                        <option value="2012">2012</option>
-                        <option value="2013">2013</option>
-                        <option value="2014">2014</option>
-                        <option value="2015">2015</option>
-                        <option value="2016">2016</option>
-                        <option value="2017">2017</option>
-                        </select>
-                    </div>
 
                     <div className="option-group">
                         <label htmlFor="leagues">League:</label>
-                        <select name="leagues" id="leagues" className="leagues">
-                        <option value="England">England</option>
-                        <option value="Spain">Spain</option>
-                        <option value="Italy">Italy</option>
-                        <option value="Germany">Germany</option>
-                        <option value="France">France</option>
+                        <select name="leagues" id="leagues" className="leagues" onChange={handleLeagueChange}>
+                        <option value="england">England</option>
+                        <option value="spain">Spain</option>
+                        <option value="italy">Italy</option>
+                        <option value="germany">Germany</option>
+                        <option value="france">France</option>
                         </select>
                     </div>
 
                     <div className="option-group">
                         <div className="game_select">
                             <label htmlFor="games">Games:</label>
-                            {/* <select name="games" id="games" multiple className="games" onChange={handleYearChange}>
-                            {games.map((game, index) => (
-                                <option key={index} value={game}>{game}</option>
-                                ))}
-                            </select>  */}
-                            <select name="games" id="games" multiple className="games" onChange={handleGamesChange}>
+                            <select name="games" id="games" className="games" onChange={handleGamesChange}>
                                 {games.map((game, index) => (
                                     <option key={index} value={game.id_odsp}>{game.ht} vs {game.at}</option>
                                 ))}
@@ -175,11 +173,8 @@ const App = () => {
                     <div className="option-group">
                         <label htmlFor="langs">Select Language:</label>
                         <select name="langs" id="langs" className="langs">
-                        <option value="english">English</option>
-                        <option value="spanish">Spanish</option>
-                        <option value="french">French</option>
-                        <option value="italian">Italian</option>
-                        <option value="german">German</option>
+                            <option value="english">English</option>
+                            <option value="french">French</option>
                         </select>
                     </div>
 
