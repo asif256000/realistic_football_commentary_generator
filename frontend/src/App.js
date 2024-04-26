49198ffd1a56297078094e065e984bab08280134
cpiyush <cpiyush854@rlogin.cs.vt.edu>
2024-04-25 23:12:56 -0400

    working audio minor style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import './App.css'; 
 import data from './generated_summaries.json';
-import myImage from './images/_2b12a71e-43f5-45b3-bafe-cf1dca77ef70.jpg'
-import image1 from './images/_4c3303d2-4e16-4254-82b7-387b756a0834.jpg';
-import image2 from './images/_070f2f46-dd49-4fca-9e0e-4d418d0662db.jpg';
-import image3 from './images/_97ae0f6e-6dee-4682-b08c-5ee731fe3092.jpg';
-import image4 from './images/_7538ab4f-31b9-4a6b-9430-f18b05f5b5d2.jpg';
-import image5 from './images/_c06113d9-90c9-41ce-83a1-ea689aef4c88.jpg';
-import image6 from './images/_f6815fda-748d-4930-9fba-6418241b8417.jpg';
-import image7 from './images/_fb64efe5-6225-45df-ab45-50076e1735c5.jpg';
 
 const OpenAI = require("openai");
 const openai = new OpenAI({
@@ -22,40 +14,78 @@ const App = () => {
     // State variables
     const [games, setGames] = useState([]);
     const [selectedLeague, setselectedLeague] = useState('england'); // Default year
-    const [stringArray, setStringArray] = useState([]);
     const [summaries, setSummaries] = useState([]);
     const [paragraphs, setParagraphs] = useState([]);
+    const [events, setEvents] = useState([]);    
+    const [elements, setElements] = useState(document.getElementsByTagName("p"));
 
+    useEffect(() =>{
+        setElements(document.getElementsByTagName("p"));
+    },[elements]);
+
+    const [gameID, setGameId] = useState('0');
+    const [lang, setLang] = useState('en');
+    const [voice, setVoice] = useState('peter');
+
+    const audio = document.getElementById("audio");
+    let audioIndex = 0;
+
+    const handleEnded = () => {
+        if (audioIndex < elements.length - 2) {
+          elements[audioIndex + 1].style.color = "black";  
+          audioIndex = audioIndex + 1;
+        //   console.log(audioIndex);
+          
+          handlePlay();
+        }
+      };
+    
+    const handlePlay = () => {
+        
+        // console.log(elements[0]);
+        audio.src = `./combined_audio/${gameID}/${voice}_${lang}_${audioIndex}_combined.wav`
+        audio.play();
+        elements[audioIndex + 1].style.color = "blue";
+    };
 
     const handleLeagueChange = (event) => {
         setselectedLeague(event.target.value);
     };
+
+    const handleLangChange = (event) => {
+        if(event.target.value === 'english'){
+            setLang('en');
+        }else{
+            setLang('fr');
+        }
+    }
+
+    const handleVoiceChange = (event) => {
+        if(event.target.value === 'vicki'){
+            setVoice('vicki');
+        }else{
+            setVoice('peter');
+        }
+    }
     
     // on click event for the generate commentary button
     const generate_commentary = async() => {
 
-        const commentaryText = stringArray.join('\n');
+        //const commentaryText = stringArray.join('\n');
 
         // Update the commentary <div> with the concatenated text
         document.querySelector(".text").style.display = "none";
         setParagraphs(String(summaries).split('\n\n'));
 
-        // try {
-        //     // Generate images for each text in stringArray
-        //     const imageUrls = await Promise.all(stringArray.map(prompt => generateImages(prompt)));
-            
-        //     // Append the generated images to the image_list div
-        //     const imageListDiv = document.querySelector(".image-list");
-        //     imageUrls.forEach(imageUrl => {
-        //         const img = document.createElement("img");
-        //         img.src = imageUrl;
-        //         img.alt = "Generated Image";
-        //         img.className = "highlight";
-        //         imageListDiv.appendChild(img);
-        //     });
-        // } catch (error) {
-        //     console.error("Error generating commentary:", error);
+
+        //await textGen(String(summaries));
+
+        // console.log(audioSrc);
+        // if(gameID === '2J6xgTqs/' || gameID === 'G29Np7eA/' || gameID === "StRC9O3T/"){
+        //     console.log(lang);
+        //     console.log(voice);
         // }
+
     }
 
     // Function to handle games select dropdown change
@@ -63,22 +93,7 @@ const App = () => {
         const selectedGameId = event.target.value;
  
         setSummaries(data[selectedGameId]);
-    
-        // Parse the filtered.csv file
-        Papa.parse("filtered.csv", {
-            download: true,
-            header: true,
-            complete: (result) => {
-                // Filter rows based on matching id_odsp and text containing "goal"
-                const matchingRows = result.data.filter(row => row.id_odsp === selectedGameId && row.text.toLowerCase().includes('goal'));
-                const texts = matchingRows.map(row => row.text);
-                setStringArray(texts);
-
-            },
-            error: (error) => {
-                console.error("Error parsing filtered.csv:", error);
-            }
-        });
+        setGameId(selectedGameId);
 
     };
     
@@ -116,21 +131,75 @@ const App = () => {
         }
     };
 
+    async function textGen(prompt) {
+        const completion = await openai.chat.completions.create({
+          messages: [
+            {
+              role: "system",
+              content: "You are a helpful assistant designed to output JSON in the format: { key_events: [ {description: }, {description: }, {description: }, {description: }, {description: }]}.",
+            },
+            { role: "user", content: "Identify 5 key events from the following game summary:  " + prompt },
+          ],
+          model: "gpt-3.5-turbo-0125",
+          response_format: { type: "json_object" },
+        });
+        console.log(completion.choices[0].message.content);
+        const result = JSON.parse(completion.choices[0].message.content);
+        setEvents(result.key_events.map(moment => moment.description)); 
+    };
+
+    async function promptgen(prompt) {
+        try {
+            const completion = await openai.chat.completions.create({
+                messages: [
+                    {
+                        role: "system",
+                        content: "Rewrite the given prompt with details such as team name, jersey colors, where the play takes place, player numbers, and crowd emotions so that the prompt can be directly passed into DALL-E3 to generate an image. And make sure to specify that the image be created in a impressionist painting style",
+                    },
+                    { role: "user", content: "The user prompt: " + prompt },
+                ],
+                model: "gpt-3.5-turbo-0125",
+            });
+            //console.log(completion.choices[0].message.content);
+            return completion.choices[0].message.content;
+        } catch (error) {
+            console.error("Error generating prompt:", error);
+            return null;
+        }
+    }
+
+    // useEffect(() => {
+    //     const generateAndAppendImages = async () => {
+    //         const imageListDiv = document.querySelector(".image-list");
+    //         imageListDiv.innerHTML = "";
+            
+    //         if (events.length > 0) {
+    //             for (let i = 0; i < events.length; i++) {
+    //                 let imgPrompt = await promptgen(events[i]);
+    //                 console.log(imgPrompt);
+    //                 let imageUrl = await generateImages(imgPrompt);
+    //                 const imageListDiv = document.querySelector(".image-list");
+    //                 const img = document.createElement("img");
+    //                 img.src = imageUrl;
+    //                 img.alt = "Generated Image";
+    //                 img.className = "highlight";
+    //                 img.style.height = "768px";
+    //                 imageListDiv.appendChild(img);
+    //             }
+    //         }
+    //     };
+    
+    //     generateAndAppendImages();
+    // }, [events]);
+
+
     return (
         <div>
             <h1 className = "title" style={{ marginLeft: '5px' }}>Fine-Tuned Football Commentary Generator</h1>
             <div className="container">
                 <div className="image-container">
                     <div className="image-list">
-                        {/* <img src={myImage} alt="Commentary Visual" className="highlight" />
-                        <img src={image1} alt="Commentary Visual" className="highlight" />
-                        <img src={image2} alt="Commentary Visual" className="highlight" />
-                        <img src={image3} alt="Commentary Visual" className="highlight" />
-                        <img src={image4} alt="Commentary Visual" className="highlight" />
-                        <img src={image5} alt="Commentary Visual" className="highlight" />
-                        <img src={image6} alt="Commentary Visual" className="highlight" />
-                        <img src={image7} alt="Commentary Visual" className="highlight" /> */}
-                        {/* Add more images here */}
+                        <img src = 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt = 'placeholder' style={{height: '550px'}}></img>
                     </div>
                 </div>
 
@@ -143,7 +212,9 @@ const App = () => {
                         <p key={index}>{paragraph}</p>
                     ))}
 
-                    <button className="play_audio">Play Audio</button>
+                    <button className="play_audio" onClick={handlePlay} autoPlay>Play Audio</button>
+                    <audio id="audio" onEnded={handleEnded}>
+                    </audio>
                 </div>
 
                 <div className="options">
@@ -151,11 +222,11 @@ const App = () => {
                     <div className="option-group">
                         <label htmlFor="leagues">League:</label>
                         <select name="leagues" id="leagues" className="leagues" onChange={handleLeagueChange}>
-                        <option value="england">England</option>
-                        <option value="spain">Spain</option>
-                        <option value="italy">Italy</option>
-                        <option value="germany">Germany</option>
-                        <option value="france">France</option>
+                            <option value="england">England</option>
+                            <option value="spain">Spain</option>
+                            <option value="italy">Italy</option>
+                            <option value="germany">Germany</option>
+                            <option value="france">France</option>
                         </select>
                     </div>
 
@@ -172,9 +243,17 @@ const App = () => {
 
                     <div className="option-group">
                         <label htmlFor="langs">Select Language:</label>
-                        <select name="langs" id="langs" className="langs">
+                        <select name="langs" id="langs" className="langs" onChange={handleLangChange}>
                             <option value="english">English</option>
                             <option value="french">French</option>
+                        </select>
+                    </div>
+
+                    <div className="option-group">
+                        <label htmlFor="voices">Select Commentator:</label>
+                        <select name="voices" id="voices" className="voices" onChange={handleVoiceChange}>
+                            <option value="peter">Peter</option>
+                            <option value="vicki">Vicki</option>
                         </select>
                     </div>
 
