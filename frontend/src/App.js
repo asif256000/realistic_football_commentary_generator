b758fe8baa3b2424a3a708fdea6e5850ab4aa8d5
Roarke-Rey <shreyas992010@gmail.com>
2024-04-26 00:38:58 -0400

    Changed the UI layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import './App.css'; 
 import data from './generated_summaries.json';
+import myImage from './images/_2b12a71e-43f5-45b3-bafe-cf1dca77ef70.jpg'
+import image1 from './images/_4c3303d2-4e16-4254-82b7-387b756a0834.jpg';
+import image2 from './images/_070f2f46-dd49-4fca-9e0e-4d418d0662db.jpg';
+import image3 from './images/_97ae0f6e-6dee-4682-b08c-5ee731fe3092.jpg';
+import image4 from './images/_7538ab4f-31b9-4a6b-9430-f18b05f5b5d2.jpg';
+import image5 from './images/_c06113d9-90c9-41ce-83a1-ea689aef4c88.jpg';
+import image6 from './images/_f6815fda-748d-4930-9fba-6418241b8417.jpg';
+import image7 from './images/_fb64efe5-6225-45df-ab45-50076e1735c5.jpg';
 
 const OpenAI = require("openai");
 const openai = new OpenAI({
@@ -103,7 +111,9 @@ const App = () => {
             download: true,
             header: true,
             complete: (result) => {
-                const sortedGames = result.data.sort((a, b) => a.season.localeCompare(b.season));
+                const sortedGames = result.data.sort((a, b) => a.season);
+                // TODO:
+                // const sortedGames = result.data.sort((a, b) => a.season.localeCompare(b.season));
                 const filteredGames = sortedGames.filter(game => game.country === selectedLeague);
                 setGames(filteredGames);
             },
@@ -193,79 +203,85 @@ const App = () => {
     // }, [events]);
 
 
+
     return (
         <div>
-            <h1 className = "title" style={{ marginLeft: '5px' }}>Fine-Tuned Football Commentary Generator</h1>
+            <h1 className = "title">Football Commentary Generator</h1>
             <div className="container">
+                <div className="options-commentary-container">
+                    
+                    <div className="options">
+                        <div className="options-1">
+                            <div className="option-group">
+                                <label htmlFor="leagues">League:</label>
+                                <select name="leagues" id="leagues" className="leagues" onChange={handleLeagueChange}>
+                                <option value="england">England</option>
+                                <option value="spain">Spain</option>
+                                <option value="italy">Italy</option>
+                                <option value="germany">Germany</option>
+                                <option value="france">France</option>
+                                </select>
+                            </div>
+
+                            <div className="option-group">
+                                <label htmlFor="langs">Select Language:</label>
+                                <select name="langs" id="langs" className="langs" onChange={handleLangChange}>
+                                    <option value="english">English</option>
+                                    <option value="french">French</option>
+                                </select>
+                            </div>
+     
+                        </div>
+
+                        <div className="options-1">
+                            <div className="option-group">
+                                <label htmlFor="voices">Select Commentator:</label>
+                                <select name="voices" id="voices" className="voices" onChange={handleVoiceChange}>
+                                    <option value="peter">Peter</option>
+                                    <option value="vicki">Vicki</option>
+                                </select>
+                            </div>
+
+                            <div className="option-group">
+                                <div className="game_select">
+                                    <label htmlFor="games">Games:</label>
+                                    <select name="games" id="games" className="games" onChange={handleGamesChange}>
+                                        {games.map((game, index) => (
+                                            <option key={index} value={game.id_odsp}>{game.ht} vs {game.at}</option>
+                                        ))}
+                                    </select> 
+                                </div> 
+                            </div>  
+                        </div>
+
+                        <div className="option-group generate-button">
+                            <input type="submit" value="Generate Commentary" className="comment_gen" onClick={generate_commentary}/>
+                        </div>
+                    </div>
+
+                    <div className="commentary">
+                        <div className="commentary-header">
+                            <h3>Commentary</h3>
+                            <button className="play_audio" onClick={handlePlay} autoPlay>Play Audio</button>
+                            <audio id="audio" onEnded={handleEnded}>
+                            </audio>
+                        </div>
+                        
+                        <p className="text" style={{ margin: '3px' }}>
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        </p>
+                        {paragraphs.map((paragraph, index) => (
+                            <p key={index}>{paragraph}</p>
+                        ))}
+
+                        
+                    </div>
+
+                </div>
                 <div className="image-container">
                     <div className="image-list">
-                        <img src = 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt = 'placeholder' style={{height: '550px'}}></img>
+                    <img src = 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt = 'placeholder'></img>
                     </div>
-                </div>
-
-                <div className="commentary">
-                    <h3>Commentary</h3>
-                    <p className="text" style={{ margin: '3px' }}>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
-                    {paragraphs.map((paragraph, index) => (
-                        <p key={index}>{paragraph}</p>
-                    ))}
-
-                    <button className="play_audio" onClick={handlePlay} autoPlay>Play Audio</button>
-                    <audio id="audio" onEnded={handleEnded}>
-                    </audio>
-                </div>
-
-                <div className="options">
-
-                    <div className="option-group">
-                        <label htmlFor="leagues">League:</label>
-                        <select name="leagues" id="leagues" className="leagues" onChange={handleLeagueChange}>
-                            <option value="england">England</option>
-                            <option value="spain">Spain</option>
-                            <option value="italy">Italy</option>
-                            <option value="germany">Germany</option>
-                            <option value="france">France</option>
-                        </select>
-                    </div>
-
-                    <div className="option-group">
-                        <div className="game_select">
-                            <label htmlFor="games">Games:</label>
-                            <select name="games" id="games" className="games" onChange={handleGamesChange}>
-                                {games.map((game, index) => (
-                                    <option key={index} value={game.id_odsp}>{game.ht} vs {game.at}</option>
-                                ))}
-                            </select> 
-                        </div> 
-                    </div>
-
-                    <div className="option-group">
-                        <label htmlFor="langs">Select Language:</label>
-                        <select name="langs" id="langs" className="langs" onChange={handleLangChange}>
-                            <option value="english">English</option>
-                            <option value="french">French</option>
-                        </select>
-                    </div>
-
-                    <div className="option-group">
-                        <label htmlFor="voices">Select Commentator:</label>
-                        <select name="voices" id="voices" className="voices" onChange={handleVoiceChange}>
-                            <option value="peter">Peter</option>
-                            <option value="vicki">Vicki</option>
-                        </select>
-                    </div>
-
-                    <div className="option-group">
-                        <input type="submit" value="Generate Commentary" className="comment_gen" onClick={generate_commentary}/>
-                    </div>
-                    </div>
-
-                <div className="upload">
-                    <label htmlFor="upload" className="upload_label">Upload Your File:</label>
-                    <br /><br />
-                    <input type="submit" value="Upload File" className="upload_button" />
                 </div>
             </div>
         </div>
